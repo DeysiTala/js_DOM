e4650d3e4adcbd1a71fb11e2635add9238b64511
Deysi <sude.90@gmail.com>
2017-07-25 12:10:59 -0500

    actualizando

---
+++ main2.js
@@ -0,0 +1,38 @@
+
+function Pasajeros(nombres,apellido,dnis,numeroAsientos){
+
+  this.nombres = nombres,
+  this.apellido = apellido,
+  this.dnis= dnis,
+  this.nAsiento=numeroAsientos
+
+ }
+function Bus(){
+
+this.numeroAsiento =[];
+
+this.add = function(datitos2){
+	this.numeroAsiento.push(datitos2);
+}
+
+ this.reservar =  function(){
+
+ }
+ 
+ this.cancelar = function(){
+ }
+
+ this.limpiar=function(){
+ }
+
+ this.buscar=function(){
+
+}
+ this.asientos=function(){
+
+ }
+
+
+
+}
+
